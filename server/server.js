0b1196682879f824da89138fd523d0db8b41ed24
Brian Biggs <brianbiggs@Brians-MacBook-Pro.local>
2018-10-23 15:45:45 -0400

    Trying to complete updates...

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.get('/todos/:id', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+  console.log(`Started up on port ${port}`);
 });
 
 module.exports = {app};
